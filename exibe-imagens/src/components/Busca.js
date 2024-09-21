ab31b71d6e3c00c423489f9b8b2c5fd7b8aab20c
Lucy Adriana <adrilucy1996@gmail.com>
2024-09-21 19:44:10 -0300

    ap07(exibe-imagens): encerra a secao 2.7

--- exibe-imagens/src/components/Busca.js
+++ exibe-imagens/src/components/Busca.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 //rcc react orientado a classe
-import { InputText } from 'primereact/inputtext'
 import { Button } from 'primereact/button'
+
+import { InputText } from 'primereact/inputtext'
+import { InputIcons } from 'primereact/inputicon'
+import { iconField} from 'primereact/iconfield'
 
 export default class busca extends Component {
 
@@ -10,30 +13,37 @@ export default class busca extends Component {
     }
 
     onTermoAlterado = (evento) => {
-        console.log(evento.target.value)
+      console.log(evento.target.value)
+      this.setState({termoDeBusca: evento.target.value})
+    }
+
+    onFormSubmit = (evento) => {
+      evento.preventDefault()
+      this.props.onBuscaRealizada(this.state.termoDeBusca)
     }
 
 
-  render() {
-    return (
-      <div className='flex flex-column'>
-        <span
-            className='p-input-icon-left w-full'>
-                <i className="pi pi-search"></i>
-                <InputText
-                    className='w-full'
-                    onChange={this.onTermoAlterado}
-                    placeholder={this.props.dica}
-                
-                />
-
-        </span>
-
-      </div>
-    )
+    render() {
+      return (
+        <form onSubmit={this.onFormSubmit}>
+          <div className='flex flex-column'>
+            <IconField iconPosition='left'>
+              <InputIcon className='pi pi-search'></InputIcon>
+              <InputText
+                value={this.state.termoDeBusca} 
+                onChange={this.onTermoAlterado}
+                className='w-full' 
+                placeholder={this.props.dica}/>
+            </IconField>
+            <Button 
+              label='OK'
+              className='p-button-outlined mt-2'/>
+          </div>
+        </form>
+      )
+    }
   }
-}
-
-Busca.defaultProps = {
+  
+  Busca.defaultProps = {
     dica: 'Digite algo que deseja ver...'
-}
+  }
--- exibe-imagens/src/components/app.js
+++ exibe-imagens/src/components/app.js
@@ -1,12 +1,30 @@
 //react arrow funcion componen export: rafce
 import React from 'react'
+import Busca from './Busca.js'
+import { createClient } from 'pexels'
+export default class App extends React.Component {
 
-const app = () => {
-  return (
-    <div>
-        <h1>Exibir uma lista de...</h1>
-    </div>
-  )
+  pexelsClient = null
+
+  componentDidMount(){
+    pexelsClient = createClient('')
+  }
+
+  onBuscaRealizada = (termo) => {
+    console.log(termo)
+  }
+
+  render(){
+    return (
+      <div
+        className='grid justify-content-center m-auto w-9 border-round border-1 border-400'>
+          <div className='col-12'>
+            <h1>Exibir uma lista de...</h1>
+          </div>
+          <div className='col-8'>
+            <Busca onBuscaRealizada={this.onBuscaRealizada}/>
+          </div>
+      </div>
+    )
+  }
 }
-
-export default app
